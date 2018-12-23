4d7330dce6f5544666746fb586a5793d1778fbb1
Moose <cooper@sudomoose.me>
2018-12-23 13:19:52 -0800

    update

--- build.js
+++ build.js
@@ -8,55 +8,53 @@ const path = require('path')
 
 // Define options for Boxen
 const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round'
+	padding: 1,
+	margin: 1,
+	borderStyle: 'round'
 }
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
+	name: chalk.white('               Cooper Makhijani'),
+	handle: chalk.white('cmakh'),
+	work: chalk.white('Developer, founder at Weatherbot. Management, botlist.space'),
+	opensource: chalk.white('FigureDB Developer ') + chalk.white('▲'),
+	twitter: chalk.yellow('https://twitter.com/') + chalk.cyan('sudomoose'),
+	npm: chalk.yellow('https://npmjs.com/') + chalk.red('~cmakh'),
+	github: chalk.yellow('https://github.com/') + chalk.green('cmakh'),
+	linkedin: chalk.yellow('https://linkedin.com/in/') + chalk.blue('bitandbang'),
+	web: chalk.cyan('https://sudomoose.me'),
+	npx: chalk.red('npx') + ' ' + chalk.white('cmakh'),
+	labelWork: chalk.white.bold('       Work:'),
+	labelTwitter: chalk.white.bold('    Twitter:'),
+	labelnpm: chalk.white.bold('        npm:'),
+	labelGitHub: chalk.white.bold('     GitHub:'),
+	labelOpenSource: chalk.white.bold('Open Source:'),
+	labelWeb: chalk.white.bold('        Web:'),
+	labelCard: chalk.white.bold('       Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
+const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+	newline + newline + // Add one whole blank line
+	working + newline + // data.labelWork + data.work
+	opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+	twittering + newline + // data.labelTwitter + data.twitter
+	npming + newline + // data.labelnpm + data.npm
+	githubing + newline + // data.labelGitHub + data.github
+	//linkedining + newline + // data.labelLinkedIn + data.linkedin
+	webing + newline + newline + // data.labelWeb + data.web
+	carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
